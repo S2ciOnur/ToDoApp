46badaee87636604f478f98adadbcca242c6fde2
xOnur <Onur_93@yahoo.de>
2018-01-25 22:46:29 +0100

    Settings New Menue with Alert Dialog.

--- app/src/main/java/com/example/mesut/todolist/activities/SettingsActivity.java
+++ app/src/main/java/com/example/mesut/todolist/activities/SettingsActivity.java
@@ -1,13 +1,15 @@
 package com.example.mesut.todolist.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mesut.todolist.R;
@@ -15,53 +17,72 @@ import com.example.mesut.todolist.R;
 public class SettingsActivity extends AppCompatActivity {
 
     private static final String TAG = "SettingsActivity";
-    private EditText userInput;
-    private TextView userOutput;
-    private Spinner choosentextSize;
+    private Button settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        initSizeSpinner();
-        this.choosentextSize = (Spinner)findViewById(R.id.spinner_sizeUnit);
-        this.userInput = (EditText)findViewById(R.id.fontsize_editText);
-        this.userOutput = (TextView)findViewById(R.id.fontsize_textView);
+        this.settingsBtn = (Button) findViewById(R.id.btnSettings);
+
     }
 
-    public void onClick(View v){
-        switch(v.getId()) {
-            case R.id.fontSize_button :
-                Log.d(TAG, "Start new Activity: ItemActivity");
-                getInputValue();
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSettings:
+                showSettingsDialog();
                 break;
-            default :
+            default:
                 Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void getInputValue(){
-      String input = userInput.getText().toString();
-      String textSizeString = choosentextSize.getSelectedItem().toString();
-        Toast.makeText(getApplicationContext(), textSizeString, Toast.LENGTH_SHORT).show();
-      try {
-          int textSize = Integer.parseInt(input);
-          userOutput.setText(input);
-      }
-      catch (RuntimeException e){
-          Toast.makeText(getApplicationContext(), "Bitte nur Integer Werte", Toast.LENGTH_SHORT).show();
-      }
-    }
 
-    private void initSizeSpinner(){
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_sizeUnit);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+    public void showSettingsDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.textsize_alert, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText userInput = (EditText) dialogView.findViewById(R.id.userInput);
+        final Spinner choosenTextSize = (Spinner) dialogView.findViewById(R.id.choosenTextSize);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.string_size_Array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        choosenTextSize.setAdapter(adapter);
 
+        dialogBuilder.setTitle("Change Text Size");
+
+        dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String textSize = userInput.getText().toString();
+                String sizeType = choosenTextSize.getSelectedItem().toString();
+                getInputValue(textSize, sizeType);
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+
+
+
+
+    private void getInputValue(String textSize, String sizeType) {
+        Toast.makeText(getApplicationContext(), textSize, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), sizeType, Toast.LENGTH_SHORT).show();
+        try {
+            int textSizeInput = Integer.parseInt(textSize);
+        } catch (RuntimeException e) {
+            Toast.makeText(getApplicationContext(), "Bitte nur Integer Werte", Toast.LENGTH_SHORT).show();
+        }
     }
 }
