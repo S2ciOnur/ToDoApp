83cac6006fd6396d5618aca22432ca10dbe32e30
xOnur <Onur_93@yahoo.de>
2018-02-18 14:15:31 +0100

    Alert shit

--- app/src/main/java/com/example/mesut/todolist/activities/MainActivity.java
+++ app/src/main/java/com/example/mesut/todolist/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mesut.todolist.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,7 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mesut.todolist.R;
@@ -36,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         dbh = new DatabaseHelper(this);
 
         todos = dbh.getAllTodos();
-        todoListAdapter = new TodoListAdapter(this,R.layout.layout_todolist, todos);
+        todoListAdapter = new TodoListAdapter(this, R.layout.layout_todolist, todos);
 
         //TODO INITLIST
         listView = (ListView) findViewById(R.id.simpleListView);
@@ -69,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, "LÖSCHE " + todos.get((int) l).getTitle() + "???" , Toast.LENGTH_SHORT).show();
-                //TODO MESUT Alertfenster (Möchten sie wirklich löschen)
+                Toast.makeText(MainActivity.this, "LÖSCHE " + todos.get((int) l).getTitle() + "???", Toast.LENGTH_SHORT).show();
+                deleteAlert();
                 return false;
             }
         });
@@ -81,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.todomenu, menu);
         return true;
@@ -89,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Für "Action" Buttons
+     *
      * @param item
      * @return
      */
@@ -106,33 +108,64 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Für alle "normalen" Buttons
+     *
      * @param v
      */
-    public void onClick(View v){
-        switch(v.getId()) {
-            case R.id.add_fab :
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_fab:
                 //TODO
                 //dbh.insertTestDataForDebug();
                 //dbh.deleteCat(1);
                 //dbh.deleteCat(0);
                 startItemActivity();
                 break;
-            default :
+            default:
                 Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT).show();
         }
     }
 
 
-    private void startSettingsActivity(){
+    private void startSettingsActivity() {
         Log.d(TAG, "Start new Activity: SettingsActivity");
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
-    private void startItemActivity(){
+    private void startItemActivity() {
         Log.d(TAG, "Start new Activity: ItemActivity");
         Intent intent = new Intent(this, ItemActivity.class);
         startActivity(intent);
+    }
+
+    private void deleteAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure to delete?");
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing but close the dialog
+
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 }
 
